d5429f3becea2928be590aca4afb37a7e738756f
Mario_Dou7 <mariomamdouhreda@gmail.com>
2022-05-09 04:03:30 +0200

    Initial Template

---
+++ Program/NPuzzle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Program
+{
+    class NPuzzle
+    {
+        static bool Solvable()  //should be bounded by O(S^2), S is the puzzle size
+        {
+            //2.	Determine whether a given state is solvable or not? 
+            throw new NotImplementedException();
+        }
+
+        static void Solve() // bounded by O(E log(V)), E is the total number of moves and V is the number of states till reaching to the solution 
+        {
+            //3.	IF SOLVABLE, apply A* search algorithm 
+
+            //4.	Print a STEP by STEP movements occur in the A* algorithms till you reach the final solvable board.
+
+            throw new NotImplementedException();
+        }
+
+        private void PrepareTree()
+        {
+            // 3.a	Convert the given puzzle into tree while applying the A* search algorithm
+            //Where each node in the search tree represents an arrangement of tiles (one board state)
+
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ Program/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.	Read in a file containing an N board with (N – 1) numbered tiles and one blank space – representing an initial state. 
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
