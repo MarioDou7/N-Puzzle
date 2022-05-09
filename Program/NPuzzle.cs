8abb166a38c4dbb6f2b0275c99aba9ad9c12cbec
Mario_Dou7 <mariomamdouhreda@gmail.com>
2022-05-09 23:38:42 +0200

    Manhatten Done

--- Program/NPuzzle.cs
+++ Program/NPuzzle.cs
@@ -5,21 +5,21 @@ using System.Text;
 
 namespace Program
 {
-    class NPuzzle
+    public static class NPuzzle
     {
-        static bool isFeasablie(char[,] boardPuzzle)  //should be bounded by O(S^2), S is the puzzle size
+        public static bool isFeasablie(char[,] boardPuzzle)  //should be bounded by O(S^2), S is the puzzle size
         {
             //2.	Determine whether a given state is solvable or not? 
             throw new NotImplementedException();
         }
 
-        static void Solve(char[,] boardPuzzle) // bounded by O(E log(V)), E is the total number of moves and V is the number of states till reaching to the solution 
+        public static void Solve(char[,] boardPuzzle) // bounded by O(E log(V)), E is the total number of moves and V is the number of states till reaching to the solution 
         {
             //3.	IF SOLVABLE, apply A* search algorithm 
 
             //4.	Print a STEP by STEP movements occur in the A* algorithms till you reach the final solvable board.
-
             throw new NotImplementedException();
+            
         }
 
 /*        private void PrepareTree()
@@ -29,11 +29,29 @@ namespace Program
 
             throw new NotImplementedException();
         }*/
-        private int Manhatten(char[,] currentState, char[,] goalState) // The sum of the distances (sum of the vertical and horizontal distance) from the blocks to their goal position + number of moves made so far to get to the state.
+        static int Manhatten(char[,] currentState) // The sum of the distances (sum of the vertical and horizontal distance) from the blocks to their goal position + number of moves made so far to get to the state.
         {
-            throw new NotImplementedException();
+            int N = (int)Math.Sqrt(currentState.Length);
+            int row_goal;
+            int col_goal;
+            int number;
+            int manhatten = 0;
+
+            for (int i = 0; i < currentState.Length; i++)
+            {
+                number = Int32.Parse(currentState[i / N, i % N].ToString());
+                if (number == 0)
+                    continue;
+                row_goal = (number-1) / N;                   //find the goal postion of the number (at which row)              
+                col_goal = (number-1) % N;                   //find the goal postion of the number (at which col)
+
+                manhatten += (Math.Abs(row_goal - i / N) + Math.Abs(col_goal - i % N));
+            }
+
+            return manhatten;
+
         }
-        private int Hamming(char[,] currentState, char[,] goalState) // The number of blocks in the wrong position + the number of moves made so far to get to the state. 
+        static int Hamming(char[,] currentState, char[,] goalState) // The number of blocks in the wrong position + the number of moves made so far to get to the state. 
         {
             throw new NotImplementedException();
         }
--- Program/Program.cs
+++ Program/Program.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using System.Diagnostics;
-
+/*using static Program.NPuzzle;
+*/
 namespace Program
 {
     class Program
@@ -15,8 +16,9 @@ namespace Program
             string[] sampleSolution_Files = { "8 Puzzle (1) - Sol", "8 Puzzle (2) - Sol", "8 Puzzle (3) - Sol", "15 Puzzle (1) - Sol", "24 Puzzle 1 (Sol)", "24 Puzzle 2 (Sol)" };
             string[] sampleUnSolvable_Files = { "8 Puzzle - Case 1", "8 Puzzle(2) - Case 1", "8 Puzzle(3) - Case 1", "15 Puzzle - Case 2", "15 Puzzle - Case 3" };
 
+            char[,] board = { { '8', '1', '3' }, { '4', '0', '2' }, { '7', '6', '5' } };
 
-
+            NPuzzle.Solve(board);
 
             Console.WriteLine("Hello World!");
         }
