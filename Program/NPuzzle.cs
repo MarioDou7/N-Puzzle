9626691c33f3fda2f4572ba176381c5bd8e62b6c
Mario_Dou7 <mariomamdouhreda@gmail.com>
2022-05-09 21:24:36 +0200

    Perparing Template

--- Program/NPuzzle.cs
+++ Program/NPuzzle.cs
@@ -7,13 +7,13 @@ namespace Program
 {
     class NPuzzle
     {
-        static bool Solvable()  //should be bounded by O(S^2), S is the puzzle size
+        static bool isFeasablie(char[,] boardPuzzle)  //should be bounded by O(S^2), S is the puzzle size
         {
             //2.	Determine whether a given state is solvable or not? 
             throw new NotImplementedException();
         }
 
-        static void Solve() // bounded by O(E log(V)), E is the total number of moves and V is the number of states till reaching to the solution 
+        static void Solve(char[,] boardPuzzle) // bounded by O(E log(V)), E is the total number of moves and V is the number of states till reaching to the solution 
         {
             //3.	IF SOLVABLE, apply A* search algorithm 
 
@@ -22,13 +22,20 @@ namespace Program
             throw new NotImplementedException();
         }
 
-        private void PrepareTree()
+/*        private void PrepareTree()
         {
             // 3.a	Convert the given puzzle into tree while applying the A* search algorithm
             //Where each node in the search tree represents an arrangement of tiles (one board state)
 
             throw new NotImplementedException();
+        }*/
+        private int Manhatten(char[,] currentState, char[,] goalState) // The sum of the distances (sum of the vertical and horizontal distance) from the blocks to their goal position + number of moves made so far to get to the state.
+        {
+            throw new NotImplementedException();
         }
-
+        private int Hamming(char[,] currentState, char[,] goalState) // The number of blocks in the wrong position + the number of moves made so far to get to the state. 
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Program/Program.cs
+++ Program/Program.cs
@@ -11,7 +11,13 @@ namespace Program
         static void Main(string[] args)
         {
             //1.	Read in a file containing an N board with (N – 1) numbered tiles and one blank space – representing an initial state. 
-            
+            string[] sampleSolveableTests_Files = { "8 Puzzle (1)", "8 Puzzle (2)", "8 Puzzle (3)", "15 Puzzle - 1", "24 Puzzle 1", "24 Puzzle 2" };
+            string[] sampleSolution_Files = { "8 Puzzle (1) - Sol", "8 Puzzle (2) - Sol", "8 Puzzle (3) - Sol", "15 Puzzle (1) - Sol", "24 Puzzle 1 (Sol)", "24 Puzzle 2 (Sol)" };
+            string[] sampleUnSolvable_Files = { "8 Puzzle - Case 1", "8 Puzzle(2) - Case 1", "8 Puzzle(3) - Case 1", "15 Puzzle - Case 2", "15 Puzzle - Case 3" };
+
+
+
+
             Console.WriteLine("Hello World!");
         }
     }
