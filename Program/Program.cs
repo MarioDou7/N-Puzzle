b78ad3ccf7d62f53546bbaa2f2a4d1eb6d0c8d0b
ZyadAshraf7 <zyadharfosh88@gmail.com>
2022-05-10 14:42:48 +0200

    isFeasibleFunctionDone/Ziad

--- Program/Program.cs
+++ Program/Program.cs
@@ -16,11 +16,16 @@ namespace Program
             string[] sampleSolution_Files = { "8 Puzzle (1) - Sol", "8 Puzzle (2) - Sol", "8 Puzzle (3) - Sol", "15 Puzzle (1) - Sol", "24 Puzzle 1 (Sol)", "24 Puzzle 2 (Sol)" };
             string[] sampleUnSolvable_Files = { "8 Puzzle - Case 1", "8 Puzzle(2) - Case 1", "8 Puzzle(3) - Case 1", "15 Puzzle - Case 2", "15 Puzzle - Case 3" };
 
-            char[,] board = { { '8', '1', '3' }, { '4', '0', '2' }, { '7', '6', '5' } };
+            //char[,] board = { { '8', '1', '3' }, { '4', '0', '2' }, { '7', '6', '5' } };
+            int[,] board = { { 2, 1, 3, 4 }, { 5, 8, 7, 6 }, { 9, 10, 12, 11 }, { 15, 13, 14, 0 } };
+            bool ok = NPuzzle.isFeasablie(board);
+            if (ok)
+                Console.WriteLine("Solvable");
+            else
+                Console.WriteLine("Not Solvable");
+            //NPuzzle.Solve(board);
 
-            NPuzzle.Solve(board);
-
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
         }
     }
 }
